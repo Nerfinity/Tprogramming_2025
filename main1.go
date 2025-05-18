bdb8b1f45a4cc3a4e352d9fd3880dd17e2e8d61a
Nerfinity <yana.kochetkova.2003@mail.ru>
2025-05-18 19:12:11 +0300

    Задача 2

--- main1.go
+++ main1.go
@@ -1,20 +1,50 @@
+// Задача 1
+//
+// //package main
+//
+//import (
+//   "fmt"
+//   "math"
+//)
+//
+//func main() {
+// Задаем значения a и b и x
+//    fmt.Println(calculate(7.2, 4.2,  2.4))
+//}
+//func calculate(a, b, x float64) float64 {
+//   numerator := math.Abs(a - b*x)
+//
+//   denominator := math.Pow(math.Log10(x), 3)
+//
+//   result := math.Sqrt(numerator / denominator)
+//
+// return result
+//}
+
+//Задача 2
+
 package main
 
 import (
-    "fmt"
-    "math"
+	"fmt"
+	"math"
 )
 
 func main() {
-    // Задаем значения a и b и x
-    fmt.Println(calculate(7.2, 4.2,  2.4))
+	fmt.Println(calculate(7.2, 4.2, 2.4))
+
+	var arr [5]float64 = [5]float64{2.4, 2.8, 3.9, 4.7, 3.16}
+	fmt.Println(arr)
+	for _, el := range arr {
+		fmt.Println(calculate(7.2, 4.2, el))
+	}
 }
 func calculate(a, b, x float64) float64 {
-    numerator := math.Abs(a - b*x)
+	numerator := math.Abs(a - b*x)
 
-    denominator := math.Pow(math.Log10(x), 3)
+	denominator := math.Pow(math.Log10(x), 3)
 
-    result := math.Sqrt(numerator / denominator)
+	result := math.Sqrt(numerator / denominator)
 
-    return result
+	return result
 }
